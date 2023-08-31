239063dd3226ce781517854db6783721dfd59c0f
Kobayashi Shunta <monta.k0523@gmail.com>
2023-08-31 15:09:28 +0900

    fix: private key parse

--- backend/lib/firebase-admin/cert.ts
+++ backend/lib/firebase-admin/cert.ts
@@ -1,6 +1,6 @@
 export const cert = {
   projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
   clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY
+  privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/gm, '\n')
 }
 console.log(cert)
